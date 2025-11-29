453f1e55d4dd56e975ed2a355e110c226d13e8df
zahra shahbazi <zahra.shahbazi.nafas@gmail.com>
2025-11-29 19:41:18 +0330

    learnin castin

---
+++ castin.py
@@ -0,0 +1,21 @@
+# moarefi khod be karbar
+print('hello im python')
+
+#soal kardn esme karbar
+x=input('salam esm shoma chist?')
+
+#esm ro chaaap miknim
+print("ohh esme to "+x+"ast che esme zibaie")
+#teadad horof esm
+print("teadad horof esmet:",len(x))
+
+# namayesh esm ba horof bozorg
+print("ba horof bozorg:",x.upper())
+
+#naayesh esm bahorof kochak
+print("ba horof kochak:",x.lower())
+
+#chop avalin horof esm
+print("avalin horof:",x[0])
+#chap akharin horof esm
+print("akharin horof:",x[-1])
